55920b3c6b6fece4cbf03588bff2ea2a19e5e011
ramprit123 <ramprit.sahani@outlook.com>
2025-04-06 13:04:45 +0530

    feat: wrap layout with context and query providers
    Wrap the main layout component with UserProvider, ThemeProvider, GestureHandlerRootView, and QueryClientProvider to enable context and query management across the app. This ensures consistent state and query handling throughout the application.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,29 @@
+import { UserProvider } from '~/context/UserContext';
 import '../global.css';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
+import { ThemeProvider } from '~/components/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        options={{
-          headerShown: false,
-        }}
-        name="welcome"
-      ></Stack.Screen>
-    </Stack>
+    <UserProvider>
+      <ThemeProvider>
+        <GestureHandlerRootView>
+          <QueryClientProvider client={queryClient}>
+            <Stack>
+              <Stack.Screen
+                options={{
+                  headerShown: false,
+                }}
+                name="welcome"
+              ></Stack.Screen>
+            </Stack>
+          </QueryClientProvider>
+        </GestureHandlerRootView>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
